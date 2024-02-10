76816f1fde8ac8bf78055b7f197727212834af88
amazon-clg <keelu.balakrishna02@gmail.com>
2024-02-10 22:41:16 +0530

    css done

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+const modeMsg = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+// modeMsg.forEach((mode) => {
+    // modeMsg.addEventListener("click", () => {
+    //     body.style.backgroundColor = "black";
+    //     body.style.color = white;
+    })
+// })
